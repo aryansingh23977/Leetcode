8c66b8af13801732a71539b8f1e6fbedbb9ea49f
ASCODES <aryansingh23977@gmail.com>
2026-05-16 08:52:38 +0000

    Reverse a number (without loop)  **Revise this problem**

--- other_Questions/lev2_01_rev.cpp
+++ other_Questions/lev2_01_rev.cpp
@@ -1,18 +1,15 @@
-//Reverse a number (without loop)  
+//Reverse a number (without loop).  **Revise this problem**
 #include <iostream>
 using namespace std;
-int rev(int i);
+int reverse(int i, int rev);
 int main() {
     int i;
     cin>>i;
-    cout<<"The Reverse number is: "<<rev(i)<<endl;
+    cout<<"The Reverse number is: "<<reverse(i,0)<<endl;
     return 0;
 }
 
-int rev(int i){
-    if(i==0){return 0;}
-    int a;
-    a=i/10;
-    return i*10
-    
+int reverse(int i, int rev){
+    if(i==0){return rev;}
+    return reverse(i/10,rev*10+i%10);
 }
